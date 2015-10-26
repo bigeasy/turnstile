d99ea6a57077ecb9e9719b95f9450dc10bec1f73
Alan Gutierrez <alan@prettyrobots.com>
2015-10-26 12:41:19 -0400

    Implement timeouts.
    Closes #65.

--- t/turnstile/turnstile.t.js
+++ t/turnstile/turnstile.t.js
@@ -1,18 +1,33 @@
-require('proof')(3, require('cadence')(prove))
+require('proof')(6, require('cadence')(prove))
 
 function prove (async, assert) {
     var abend = require('abend')
+    var wait
     var object = {
-        goodness: function (value, callback) {
+        block: function (state, value, callback) {
+            wait = callback
+        },
+        goodness: function (state, value, callback) {
             callback(null, value * 2)
         },
-        badness: function (value, callback) {
+        badness: function (state, value, callback) {
             callback(new Error('badness'))
+        },
+        timedout: function (state, value, callback) {
+            callback(null, state.timedout)
         }
     }
     var Turnstile = require('../..')
-    var turnstile = new Turnstile
+    var turnstile = new Turnstile, now = 0
     assert(turnstile.workers, 1, 'default constructor')
+    turnstile = new Turnstile({
+        timeout: 1,
+        _Date: {
+            now: function () {
+                return now
+            }
+        }
+    })
     async(function () {
         var done = async()
         turnstile.enter(object, object.goodness, [ 1 ], function (error, result) {
@@ -23,6 +38,25 @@ function prove (async, assert) {
             assert(error.message, 'badness', 'catch error')
             done()
         })
+        turnstile.nudge(abend)
+    }, function () {
+        var done = async(), count = 0
+        turnstile.enter(object, object.goodness, [ 1 ], function (error, result) {
+            assert(result, 'timedout')
+            if (++count == 3) done()
+        })
+        turnstile.nudge(abend)
+        turnstile.enter(object, object.timedout, [ 1 ], function (error, result) {
+            assert(result, 'timedout')
+            if (++count == 3) done()
+        })
+        turnstile.nudge(abend)
+        now = 1
+        turnstile.enter(object, object.goodness, [ 1 ], function (error, result) {
+            assert(result, 2, 'did not timeout')
+            if (++count == 3) done()
+        })
+        now = 2
         turnstile.nudge(abend)
     })
 }
--- turnstile.js
+++ turnstile.js
@@ -7,10 +7,14 @@ function Turnstile (options) {
     this.working = 0
     this.workers = options.workers || 1
     this.waiting = 0
+    this.rejecting = 0
+    this.timeout = options.timeout || Infinity
+    this._Date = options._Date || Date
 }
 
 Turnstile.prototype.enter = function (object, method, vargs, callback) {
     var task = {
+        when: this._Date.now(),
         object: object,
         method: method,
         vargs: vargs,
@@ -23,24 +27,42 @@ Turnstile.prototype.enter = function (object, method, vargs, callback) {
     this.waiting++
 }
 
+Turnstile.prototype._stopWorker = function () {
+    return this.waiting == 0
+}
+
+Turnstile.prototype._stopRejector = function () {
+    return this.waiting == 0
+        || this._Date.now() - this._head.next.when <= this.timeout
+}
+
 // We use Cadence because of its superior try/catch abilities.
-Turnstile.prototype._nudge = cadence(function (async) {
+Turnstile.prototype._nudge = cadence(function (async, counter, stopper) {
+    var severed = false
     async([function () {
-        this.working--
+        this[counter]--
     }], function () {
-        this.working++
-        setImmediate(async()) // <- price, we only pay it to start work.
+        this[counter]++
     }, function () {
         var loop = async(function () {
-            if (this.waiting == 0) {
+            if (this[stopper]()) {
                 return [ loop.break ]
             }
             var task = this._head.next
             this._head.next = task.next
             this._head.next.previous = this._head
             this.waiting--
-            async([function () {
-                task.method.apply(task.object, task.vargs.concat(async()))
+            async(function () {
+                if (!severed) {
+                    severed = true
+                    setImmediate(async()) // <- price, we only pay it to start work.
+                }
+            }, [function () {
+                task.method.apply(task.object, [{
+                    turnstile: this,
+                    timedout: !! (this._Date.now() - task.when),
+                    when: task.when
+                }].concat(task.vargs, async()))
             }, function (error) {
                 (task.callback)(error)
                 return [ loop.continue ]
@@ -53,7 +75,10 @@ Turnstile.prototype._nudge = cadence(function (async) {
 
 Turnstile.prototype.nudge = function (callback) {
     while (this.waiting && this.working < this.workers) {
-        this._nudge(callback)
+        this._nudge('working', '_stopWorker', callback)
+    }
+    if (this.waiting && !this.rejecting && this._Date.now() - this._head.next.when > this.timeout) {
+        this._nudge('rejecting', '_stopRejector', callback)
     }
 }
 
